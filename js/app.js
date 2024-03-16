68988fe03969ba7e19f3ff4db2376fea9176b662
Francesca-Barletta <wallebar@gmail.com>
2024-03-16 19:12:22 +0100

    add css and result control

--- js/app.js
+++ js/app.js
@@ -1,65 +1,73 @@
-console.log('mastermind')
-
-//let randomNumbers = []
-//    while(randomNumbers.length < 4){
-//        let randomNumber = Math.floor(Math.random()* 9) + 1;
-//        if(!randomNumbers.includes(randomNumber)){
-//            randomNumbers.push(randomNumber)
-//        }
-//    }
-
-//function getRandomArbitrary(min, max) {
-//    return Math.random() * (max - min) + min;
-//}
-//const randomNumbers = getRandomArbitrary(1, 9)
+console.log('mastermind');
 
 
-//funzione che genera una array di 4 numeri univoci
-//min num minimo, max num massimo, count quanti num voglio
-function randomNumbersArray(min, max, count){
-    let randomNumbers = [];
-    while(randomNumbers.length < count){
-        let randomNumber = Math.floor(Math.random()* max) + min;
-        if(!randomNumbers.includes(randomNumber)){
-            randomNumbers.push(randomNumber);
-        }
-    }return randomNumbers;
-}
+//recupero input utente
+const inputDomElement = document.getElementById('number');
+//recuper pulsante gioca
+const sendButtomElement = document.getElementById('gioca');
+//recupero dic con id result
+const resultDivElement = document.getElementById('result')
+//genero array di 4 num random da 1 a 9
 const arrayNumbers = randomNumbersArray(1, 9, 4);
 console.log(arrayNumbers);
-const inputDomElement = document.getElementById('number');
-const sendButtomElement = document.getElementById('gioca');
-//const resultDivElement = document.getElementById('result')
-let userNumbers;
-sendButtomElement.addEventListener('click', function(){
-    const resultDivElement = document.getElementById('result')
-    userNumbers = inputDomElement.value;
+//metto una funzione al click del pulsante gioca
+sendButtomElement.addEventListener('click', function () {
+    //recupro l'input dell'utente
+    const userNumbers = inputDomElement.value;
+    //converto i valori dell'utente in una stringa di numeri interi
     const userNumberArray = convertStringToNumberArray(userNumbers);
-    console.log(userNumberArray);
-    let counter = 0
-   for(let index = 0; index < userNumberArray.length; index++){
-    const indexChecked = arrayNumbers.indexOf(userNumberArray[index])
-    console.log(indexChecked)
-    if(indexChecked !== (- 1)){
-        counter++;  
-        
+    //azzero l'input
+    inputDomElement.value = "";
+    //cerco i numeri nei due array
+    const validNumbers = findArrayNumbers(userNumberArray, arrayNumbers);
+    //se sono uguali e alla stessa posizione l'utente ha vinto
+    if(validNumbers === "xxxx"){
+        resultDivElement.innerHTML += `<div class="result"><div>${userNumbers}</div><div><span>"HAI VINTO"</span></div><div>${validNumbers}</div></div>`
+        console.log('hai vinto');
+    }else{
+        resultDivElement.innerHTML += `<div class="result"><div>${userNumbers}</div><div></div><div>${validNumbers}</div></div>`
     }
-   }console.log(`hai trovato ${counter} numeri`)
-   resultDivElement.innerHTML += `<p>hai inserito ${userNumbers} / hai trovato ${counter} numeri</p>`
+
 
 
 });
 
+//funzione per trovare i numeri dell'utente in quelli del pc
+function findArrayNumbers(userArray, consoleNumbers) {
+    let result = ""
+    for (let index = 0; index < userArray.length; index++) {
+        const indexNumber = consoleNumbers.indexOf(userArray[index]);
+        if (indexNumber !== -1) {
+            if (indexNumber === index) {
+                result += "x";
+                console.log(result);
+            } else {
+                result += "o";
+                console.log(result);
+            }
+        }
+    }
+    return result;
+}
 
-
-
-
-function convertStringToNumberArray(numberString){
-    let userArray = []
-    for(let i = 0; i < numberString.length; i++){
+//funzione per convertire la stringa in array
+function convertStringToNumberArray(numberString) {
+    let userArray = [];
+    for (let i = 0; i < numberString.length; i++) {
         //console.log(userNumbers[i]);
         userArray.push(parseInt(numberString[i]));
-        
+
     }
-    return userArray
+    return userArray;
+
+}
+//funzione per creare array di tot numeri random unici
+function randomNumbersArray(min, max, count) {
+    let randomNumbers = [];
+    while (randomNumbers.length < count) {
+        let randomNumber = Math.floor(Math.random() * max) + min;
+        if (!randomNumbers.includes(randomNumber)) {
+            randomNumbers.push(randomNumber);
+        }
+    } return randomNumbers;
 }
